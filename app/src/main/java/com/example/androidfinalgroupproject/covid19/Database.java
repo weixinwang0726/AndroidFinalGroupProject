a65ef846f8d1733c61b01386167455c9faa23796
yan00043 <yan00043@algonquinlive.com>
2020-12-04 01:26:43 -0500

    add covied19

--- app/src/main/java/com/example/androidfinalgroupproject/covid19/Database.java
+++ app/src/main/java/com/example/androidfinalgroupproject/covid19/Database.java
@@ -4,16 +4,17 @@ public class Database {
     private String country;
     private String date;
     private long id;
+    private String province;
+    private String covidcase;
 
-    //constructor
-    public Database()
-    {
+    public Database() {
     }
 
-    public Database( String country, String date )
-    {
+    public Database( String country, String date, String province, String covidcase ) {
         this.country = country;
         this.date = date;
+        this.province = province;
+        this.covidcase = covidcase;
     }
 
     public String getCountry() {
@@ -37,5 +38,18 @@ public class Database {
         this.id = id;
     }
 
+    public String getProvince() {
+        return this.province;
+    }
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCovidcase(){
+        return this.covidcase;
+    }
+    public void setCovidcase(String covidcase) {
+        this.covidcase = covidcase;
+    }
 
 }
